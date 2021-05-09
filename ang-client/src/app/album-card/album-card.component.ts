ee599baa5c27dcacd692ca2ed5fa99215c759b5f
Brendan <bh247484@gmail.com>
2021-05-09 11:00:10 -0400

    Abstract out album-card, introduce material-ui

---
+++ ang-client/src/app/album-card/album-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { albumType } from '../fakeData/fakeData';
+
+@Component({
+  selector: 'app-album-card',
+  templateUrl: './album-card.component.html',
+  styleUrls: ['./album-card.component.css']
+})
+export class AlbumCardComponent implements OnInit {
+  @Input() album: albumType;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.album)
+  }
+
+}
--- ang-client/src/app/app.module.ts
+++ ang-client/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialCompsModule } from './material-comps.module';
 
 import { AppComponent } from './app.component';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AlbumListComponent } from './album-list/album-list.component';
 import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
 import { NewAlbumFormComponent } from './new-album-form/new-album-form.component';
 import { ShowAlbumComponent } from './show-album/show-album.component';
 import { UpdateAlbumFormComponent } from './update-album-form/update-album-form.component';
+import { AlbumCardComponent } from './album-card/album-card.component';
 
 @NgModule({
   declarations: [
@@ -20,12 +22,14 @@ import { UpdateAlbumFormComponent } from './update-album-form/update-album-form.
     NewAlbumFormComponent,
     ShowAlbumComponent,
     UpdateAlbumFormComponent,
+    AlbumCardComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    FormsModule
-    // BrowserAnimationsModule
+    FormsModule,
+    MaterialCompsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
